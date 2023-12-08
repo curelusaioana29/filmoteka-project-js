c60595b97039055f895593c0d617652f8c513dd1
curelusaioana29 <136342239+curelusaioana29@users.noreply.github.com>
2023-12-08 19:22:06 +0200

    commit

---
+++ src/java-script/modal.js
@@ -0,0 +1,37 @@
+// import './fetch';
+import './refs';
+import refs from './refs';
+
+function updateModalValue(obj) {
+  refs.modalTitle.textContent = obj.title;
+  refs.modalImg.src = obj.poster_path
+    ? `https://image.tmdb.org/t/p/w500/${obj.poster_path}`
+    : './images/default-opt.jpg';
+  refs.rate.textContent = obj.vote_average;
+  refs.votes.textContent = obj.vote_count;
+  refs.popularity.textContent = obj.popularity;
+  refs.title.textContent = obj.original_title;
+  refs.genre.textContent = genresSep(obj);
+  refs.descr.textContent = obj.overview;
+
+  refs.addToWatchedBtn.dataset.id = obj.id;
+  refs.addToQueueBtn.dataset.id = obj.id;
+  genresSep(obj);
+  alignHeight();
+}
+
+
+function alignHeight() {
+  const item = refs.originalTitle;
+  const valueHeight = refs.title.offsetHeight;
+  if (item.offsetHeight !== valueHeight) {
+    item.style = `margin-bottom: ${valueHeight - 15}px`;
+  }
+}
+
+function genresSep(object) {
+  const genresArray = object.genres;
+  genresArray.map(({ name }) => (refs.genre.textContent += `${name} `));
+}
+
+export default updateModalValue;
---
+++ src/java-script/request.js
@@ -0,0 +1,10 @@
+const requstName = {
+    HOME: 'home',
+    SEARCH: 'search',
+    WATCHED: 'watched',
+    QUEUE: 'queue',
+  };
+  const { HOME, SEARCH, WATCHED, QUEUE } = requstName;
+  
+  export { HOME, SEARCH, WATCHED, QUEUE };
+  
