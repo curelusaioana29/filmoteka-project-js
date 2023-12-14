8c4e8df45cdba97ecca1d92db17df47800e95629
VallentinX <135710298+VallentinX@users.noreply.github.com>
2023-12-14 15:21:11 +0200

    19th commit

--- src/javascript/darkmode.js
+++ src/javascript/darkmode.js
@@ -1,35 +1,31 @@
 /** Dark / Light Mode */
-export const toggleDarkMode = () => {
-  const HEAD = document.getElementsByTagName('head')[0];
-  let headLink = document.createElement('link');
-
+export function initializeDarkModeToggle() {
   const BUTTON_DARK_MODE = document.getElementsByClassName('dark-mode')[0];
-  let svgDarkModeChild = document.getElementsByClassName('dark-mode')[0];
+  let svgDarkModeChild = document.getElementsByClassName('dark-mode')[2];
+  const bodyStyle = document.getElementsByTagName("body")[0];
 
   let darkModeOn = function () {
-    if (BUTTON_DARK_MODE.className === 'dark-mode activate') {
-      HEAD.appendChild(headLink);
-      headLink.rel = 'stylesheet';
-      headLink.href = './css/darkmode.css';
-
-      svgDarkModeChild.setAttribute(
-        'href',
-        './images/filmoteka-icons.svg#icon-light-mode'
-      );
-
-      BUTTON_DARK_MODE.className = 'dark-mode';
-    } else {
-      HEAD.removeChild(headLink);
-
+    if (BUTTON_DARK_MODE.classList.contains('active')) {
       svgDarkModeChild.setAttribute(
         'href',
         './images/filmoteka-icons.svg#icon-dark-mode'
       );
 
-      BUTTON_DARK_MODE.className = 'dark-mode activate';
+      BUTTON_DARK_MODE.classList.remove('active');
+
+      bodyStyle.setAttribute('style', 'background-color: #000000dd');
+    } else {
+      svgDarkModeChild.setAttribute(
+        'href',
+        './images/filmoteka-icons.svg#icon-light-mode'
+      );
+
+      BUTTON_DARK_MODE.classList.add('active');
+
+      bodyStyle.setAttribute('style', 'background-color: #ffffffdd');
     }
   };
 
   BUTTON_DARK_MODE.onclick = darkModeOn;
-};
-toggleDarkMode();
+}
+initializeDarkModeToggle();
