cdee35b858b2927478558614ffd588ac4dcb2096
curelusaioana29 <136342239+curelusaioana29@users.noreply.github.com>
2023-12-15 17:34:42 +0200

    clear

--- src/index.js
+++ src/index.js
@@ -5,36 +5,11 @@ import './javascript/api.js';
 import './javascript/darkmode.js';
 import './javascript/header-bkg.js';
 import './javascript/letters-jump.js';
-import './javascript/storage.js';
-// import './javascript/key-words.js';
-import './javascript/change-header.js';
-// import './javascript/btnLibrary.js';
-// import './javascript/clear-movie-list.js';
-//  import './javascript/library-list-hendler.js';
-
 import './javascript/main.js';
 import './javascript/notification.js';
-// import './javascript/request.js';
+
 // import './javascript/spinner.js';
-// import fnFetch from './javascript/fetch.js';
-// import fnHendler from './javascript/fnHendler.js';
-// import './javascript/fetch.js';
-// save('currentRequest', HOME);
-import { HOME } from './javascript/request.js';
-import { save } from './javascript/storage.js';
-// import './javascript/library-list-hendler.js';
-// import './javascript/clear-movie-list.js';
 
-save('currentRequest', HOME);
 
-// fnFetch.fetchData();
 
-// refs.searchForm.addEventListener('submit', fnHendler.onSubmitSearchForm);
-// refs.paginationBox.addEventListener('click', fnHendler.onClickPaginate);
-// refs.filmListRef.addEventListener('click', fnHendler.onClickFilm);
-refs.libraryHeaderBtn.addEventListener('click', fnHendler.onClickLibrary);
-refs.watchedBtn.addEventListener('click', fnHendler.onClickWatched);
-refs.queueBtn.addEventListener('click', fnHendler.onClickQueue);
-// refs.logoNav.addEventListener('click', fnHendler.onClickLogoHome);
-refs.homeHeaderBtn.addEventListener('click', fnHendler.onClickLogoHome);
 
