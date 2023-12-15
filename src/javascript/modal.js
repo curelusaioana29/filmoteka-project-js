f6a8fe071f240f6fe3180c95a511fd10456cddab
VallentinX <135710298+VallentinX@users.noreply.github.com>
2023-12-15 22:16:34 +0200

    1st commit

--- src/javascript/modal.js
+++ src/javascript/modal.js
@@ -1,26 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector(".open-review-modal"),
+    closeModalBtn: document.querySelector(".close-review-modal"),
+    modal: document.querySelector(".review-modal"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  };
-})();
-const refs = {
-  // ... other references
-  modal: document.querySelector("[data-modal]"),
-};
-
-document.body.addEventListener("click", function(event) {
-  if (event.target.matches("[data-modal-open]")) {
-    toggleModal("open-lightbox");
-  } else if (event.target.matches("[data-modal-close]")) {
-    toggleModal("close-lightbox");
+    refs.modal.classList.toggle("review-is-hidden");
   }
-});
+})();
