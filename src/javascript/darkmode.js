3d28fff876ba3f740213028221332a7085fe8356
curelusaioana29 <136342239+curelusaioana29@users.noreply.github.com>
2023-12-17 17:25:19 +0200

    Update darkmode.js

--- src/javascript/darkmode.js
+++ src/javascript/darkmode.js
@@ -1,22 +1,24 @@
 export function initializeDarkModeToggle() {
   const BUTTON_DARK_MODE = document.getElementsByClassName('dark-mode')[0];
-  let svgDarkModeChild = document.getElementsByClassName('dark-mode')[2];
+  const svgDarkModeChild = document.getElementsByClassName('dark-mode')[2];
   const bodyStyle = document.getElementsByTagName('body')[0];
   const arrowsLink = document.querySelectorAll('#arrow-link');
   const arrowsSvg = document.querySelectorAll('#arrow-svg');
 
   // Function to set dark mode
   let setDarkMode = function () {
-    svgDarkModeChild.setAttribute(
-      'href',
-      '/filmoteka-project-js/filmoteka-icons.f7e9ceb7.svg#icon-dark-mode'
-    );
+    if (svgDarkModeChild.tagName === 'use') { // Check if it's an 'use' element
+      svgDarkModeChild.setAttribute(
+        'href',
+        '/filmoteka-project-js/filmoteka-icons.f7e9ceb7.svg#icon-dark-mode'
+      );
+    }
 
     BUTTON_DARK_MODE.classList.remove('active');
 
     bodyStyle.setAttribute('style', 'background-color: #000000dd');
-    arrowsLink.setAttribute('style', 'border: 1px solid #fff');
-    arrowsSvg.setAttribute('style', 'color: #fff');
+    arrowsLink.forEach(link => link.setAttribute('style', 'border: 1px solid #fff'));
+    arrowsSvg.forEach(svg => svg.setAttribute('style', 'color: #fff'));
 
     // Save user preference in localStorage
     localStorage.setItem('darkMode', 'on');
@@ -24,16 +26,18 @@ export function initializeDarkModeToggle() {
 
   // Function to set light mode
   let setLightMode = function () {
-    svgDarkModeChild.setAttribute(
-      'href',
-      '/filmoteka-project-js/filmoteka-icons.f7e9ceb7.svg#icon-light-mode'
-    );
+    if (svgDarkModeChild.tagName === 'use') { // Check if it's an 'use' element
+      svgDarkModeChild.setAttribute(
+        'href',
+        '/filmoteka-project-js/filmoteka-icons.f7e9ceb7.svg#icon-light-mode'
+      );
+    }
 
     BUTTON_DARK_MODE.classList.add('active');
 
     bodyStyle.setAttribute('style', 'background-color: #ffffffdd');
-    arrowsLink.setAttribute('style', 'border: 1px solid #000');
-    arrowsSvg.setAttribute('style', 'color: #000');
+    arrowsLink.forEach(link => link.setAttribute('style', 'border: 1px solid #000'));
+    arrowsSvg.forEach(svg => svg.setAttribute('style', 'color: #000'));
 
     // Save user preference in localStorage
     localStorage.setItem('darkMode', 'off');
@@ -59,4 +63,5 @@ export function initializeDarkModeToggle() {
   // Event listener for dark mode toggle button
   BUTTON_DARK_MODE.onclick = toggleDarkMode;
 }
+
 initializeDarkModeToggle();
