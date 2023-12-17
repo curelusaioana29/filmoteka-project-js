184f28d13b22bb0f39be9bbb38991bec46372e1c
curelusaioana29 <136342239+curelusaioana29@users.noreply.github.com>
2023-12-17 19:30:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import refs from './javascript/refs.js';
 import './javascript/up-dwn-btn.js';
 import './javascript/api.js';
-// import './javascript/darkmode.js';
+import './javascript/darkmode.js';
 import './javascript/header-bkg.js';
 import './javascript/letters-jump.js';
 import './javascript/main.js';
