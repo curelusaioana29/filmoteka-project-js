bb8682c6c7da5ff18ddddf91943cb64af78ab056
VallentinX <135710298+VallentinX@users.noreply.github.com>
2023-12-18 16:56:33 +0200

    3rd Update

--- src/javascript/header-bttns.js
+++ src/javascript/header-bttns.js
@@ -1,24 +1,24 @@
-// script.js
-
 export function handleButtonClicks() {
-  // Get references to the elements
   const libraryButton = document.querySelector('.library');
   const homeButton = document.querySelector('.home');
   const formDiv = document.querySelector('.form');
   const userListDiv = document.querySelector('.user-list');
+  const movieListDiv = document.querySelector('#movieList');
+  const paginationDiv = document.querySelector('#pagination');
 
-  // Add click event listener to the library button
   libraryButton.addEventListener('click', function () {
-    // Toggle classes for form and user-list
     formDiv.classList.add('hidden');
     userListDiv.classList.remove('hidden');
+    movieListDiv.classList.add('hidden');
+    paginationDiv.classList.add('hidden');
   });
 
-  // Add click event listener to the home button
   homeButton.addEventListener('click', function () {
-    // Toggle classes for form and user-list
     formDiv.classList.remove('hidden');
     userListDiv.classList.add('hidden');
+    movieListDiv.classList.remove('hidden');
+    paginationDiv.classList.remove('hidden');
   });
 }
+
 handleButtonClicks();
