09c1cda86c92f576027a68218d426de2fb0f5072
RamonaMonica <ramonamonica08@icloud.com>
2023-12-18 22:04:40 +0200

    COMMIT SHOWN

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -141,8 +141,24 @@ document.addEventListener('DOMContentLoaded', async () => {
     detailsContainer.appendChild(rightSection);
     modalDetails.appendChild(detailsContainer);
 
+    const watchedButton = document.getElementById('watchedButton');
+    const queueButton = document.getElementById('queueButton');
+  
+    // Check if the movie is already in the watched list
+    const isWatched = isMovieInWatched(details.id);
+    watchedButton.textContent = isWatched ? 'REMOVE FROM WATCHED' : 'ADD TO WATCHED';
+  
+    // Check if the movie is already in the queue list
+    const isQueued = isMovieInQueue(details.id);
+    queueButton.textContent = isQueued ? 'REMOVE FROM QUEUE' : 'ADD TO QUEUE';
+  
+    // Add click event listeners for the watched and queue buttons
+    watchedButton.addEventListener('click', () => handleWatchedButtonClick(details));
+    queueButton.addEventListener('click', () => handleQueueButtonClick(details));
+  
     movieModal.style.display = 'block';
   }
+  
 
   function renderPagination(totalMovies) {
     const totalPages = Math.ceil(totalMovies / moviesPerPage);
@@ -236,6 +252,76 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     renderPagination(movies.length);
   }
+
+    // Function to check if a movie is in the watched list
+    function isMovieInWatched(movieId) {
+      const watchedMovies = JSON.parse(localStorage.getItem('watchedMovies')) || [];
+      return watchedMovies.includes(movieId);
+    }
+  
+    // Function to add a movie to the watched list
+    function addMovieToWatched(movieId) {
+      const watchedMovies = JSON.parse(localStorage.getItem('watchedMovies')) || [];
+      watchedMovies.push(movieId);
+      localStorage.setItem('watchedMovies', JSON.stringify(watchedMovies));
+    }
+  
+    // Function to remove a movie from the watched list
+    function removeMovieFromWatched(movieId) {
+      let watchedMovies = JSON.parse(localStorage.getItem('watchedMovies')) || [];
+      watchedMovies = watchedMovies.filter(id => id !== movieId);
+      localStorage.setItem('watchedMovies', JSON.stringify(watchedMovies));
+    }
+
+      function handleWatchedButtonClick(details) {
+    const watchedButton = document.getElementById('watchedButton');
+    const isCurrentlyWatched = isMovieInWatched(details.id);
+
+    if (isCurrentlyWatched) {
+      // Remove from watched list
+      removeMovieFromWatched(details.id);
+      watchedButton.textContent = 'ADD TO WATCHED';
+    } else {
+      // Add to watched list
+      addMovieToWatched(details.id);
+      watchedButton.textContent = 'REMOVE FROM WATCHED';
+    }
+  }
+
+    function isMovieInQueue(movieId) {
+      const queuedMovies = JSON.parse(localStorage.getItem('queuedMovies')) || [];
+      return queuedMovies.includes(movieId);
+    }
+  
+    // Function to add a movie to the queue list
+    function addMovieToQueue(movieId) {
+      const queuedMovies = JSON.parse(localStorage.getItem('queuedMovies')) || [];
+      queuedMovies.push(movieId);
+      localStorage.setItem('queuedMovies', JSON.stringify(queuedMovies));
+    }
+  
+    // Function to remove a movie from the queue list
+    function removeMovieFromQueue(movieId) {
+      let queuedMovies = JSON.parse(localStorage.getItem('queuedMovies')) || [];
+      queuedMovies = queuedMovies.filter(id => id !== movieId);
+      localStorage.setItem('queuedMovies', JSON.stringify(queuedMovies));
+    }
+  
+    // Function to handle queue button click
+    function handleQueueButtonClick(details) {
+      const queueButton = document.getElementById('queueButton');
+      const isCurrentlyQueued = isMovieInQueue(details.id);
+  
+      if (isCurrentlyQueued) {
+        // Remove from queue list
+        removeMovieFromQueue(details.id);
+        queueButton.textContent = 'ADD TO QUEUE';
+      } else {
+        // Add to queue list
+        addMovieToQueue(details.id);
+        queueButton.textContent = 'REMOVE FROM QUEUE';
+      }
+    }
 
   await performSearch('', currentPage);
 });
