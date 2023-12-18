bd620584c78f2e4345d748689507dff5daa22cfd
VallentinX <135710298+VallentinX@users.noreply.github.com>
2023-12-18 16:38:44 +0200

    Update Commit

--- src/javascript/up-dwn-btn.js
+++ src/javascript/up-dwn-btn.js
@@ -1,14 +1,14 @@
 export function initializeScrollButtons() {
   document.addEventListener('DOMContentLoaded', function () {
-    var topArrow = document.querySelector('.arrows a[href="#top"]');
-    var bottomArrow = document.querySelector('.arrows a[href="#bottom"]');
+    const topArrow = document.querySelector('.arrows a[href="#top"]');
+    const bottomArrow = document.querySelector('.arrows a[href="#bottom"]');
 
-    var middleOfPage = window.innerHeight / 2;
+    const middleOfPage = window.innerHeight / 2;
 
-    var prevScrollPosition = window.scrollY;
+    const prevScrollPosition = window.scrollY;
 
     function handleScroll() {
-      var scrollPosition = window.scrollY;
+      const scrollPosition = window.scrollY;
 
       if (scrollPosition > prevScrollPosition) {
         topArrow.style.display = 'flex';
