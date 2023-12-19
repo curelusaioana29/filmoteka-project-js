b80ab9619a06e12e251a282e17f044f8bfcd64b1
curelusaioana29 <136342239+curelusaioana29@users.noreply.github.com>
2023-12-19 16:22:44 +0200

    commit

--- src/javascript/spinner.js
+++ src/javascript/spinner.js
@@ -1,10 +1,42 @@
 export default {
-    spinnerShow() {
-      refs.spinner.classList.remove('is-hidden__spinner');
-    },
-  
-    spinnerClose() {
-      refs.spinner.classList.add('is-hidden__spinner');
-    },
-  };
-  
+  spinnerShow() {
+    // Show the spinner
+    const spinner = document.getElementById('spinner');
+    spinner.classList.remove('is-hidden__spinner');
+
+    // Log to the console
+    console.log('Spinner shown');
+  },
+
+  spinnerClose() {
+    // Hide the spinner
+    const spinner = document.getElementById('spinner');
+    spinner.classList.add('is-hidden__spinner');
+
+    // Log to the console
+    console.log('Spinner hidden');
+  },
+};
+// Import the spinner utility
+// import spinner from './spinner'; // Adjust the path based on your file structure
+
+// ... Your existing code ...
+
+// Example usage in your code:
+async function performSearch(searchQuery, page = 1) {
+  try {
+    // Show the spinner while performing the search
+    spinner.spinnerShow();
+
+    // Your existing search logic
+
+    // Simulate an asynchronous operation (replace this with your actual search logic)
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    // Hide the spinner after the search is complete
+    spinner.spinnerClose();
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+}
+
